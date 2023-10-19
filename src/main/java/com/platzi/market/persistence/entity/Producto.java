078b9e177a9321dcbe3c6ab643cd8dc5f5aa9886
Andres Herrera <andrherr@bancolombia.com.co>
2023-10-19 06:44:06 -0500

    saañadirparacion de capas dominio y persistencia, ta tambien se añadio el controlador

--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -4,21 +4,32 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "productos")
-public class producto {
+public class Producto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
+
     private String nombre;
+
     @Column(name = "id_categoria")
     private Integer idCategoria;
+
     @Column(name = "codigo_barras")
     private String codigoBarras;
+
     @Column(name = "precio_venta")
     private Double precioVenta;
+
     @Column(name = "cantidad_stock")
     private Integer cantidadStock;
+
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
 
     public Integer getIdProducto() {
         return idProducto;
@@ -74,5 +85,13 @@ public class producto {
 
     public void setEstado(Boolean estado) {
         this.estado = estado;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
 }
